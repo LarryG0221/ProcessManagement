6274d99e8299ea17b8e9091f14cc14f1318732ca
SandyLinux <mailliuping@gmail.com>
2020-03-29 15:42:00 -0400

    1

--- processWithLock2.py
+++ processWithLock2.py
@@ -2,31 +2,25 @@
 # the concept of race condition 
 # in multiprocessing 
 import multiprocessing 
-from multiprocessing import  Lock
 
 # function to withdraw from account 
-def withdraw(lock, balance):
-    lock.acquire()
-
-    for _ in range(10000):
-	    balance.value = balance.value - 1
-    lock.release()
+def withdraw(balance):	 
+	for _ in range(10000): 
+		balance.value = balance.value - 1
 
 # function to deposit to account 
-def deposit(lock, balance):
-    lock.acquire()
-    for _ in range(100):
-        balance.value = balance.value + 1
-    lock.release()
+def deposit(balance):	 
+	for _ in range(10000): 
+		balance.value = balance.value + 1
 
-def perform_transactions(lock): 
+def perform_transactions(): 
 
 	# initial balance (in shared memory) 
-	balance = multiprocessing.Value('i',14)
+	balance = multiprocessing.Value('i', 100) 
 
 	# creating new processes 
-	p1 = multiprocessing.Process(target=withdraw, args=(lock, balance,)) 
-	p2 = multiprocessing.Process(target=deposit, args=(lock, balance,)) 
+	p1 = multiprocessing.Process(target=withdraw, args=(balance,)) 
+	p2 = multiprocessing.Process(target=deposit, args=(balance,)) 
 
 	# starting processes 
 	p1.start() 
@@ -39,9 +33,8 @@ def perform_transactions(lock):
 	# print final balance 
 	print("Final balance = {}".format(balance.value)) 
 
-if __name__ == "__main__":
-    lock = Lock()
-    for _ in range(10): 
+if __name__ == "__main__": 
+	for _ in range(10): 
 
-        # perform same transaction process 10 times 
-        perform_transactions(lock) 
+		# perform same transaction process 10 times 
+		perform_transactions() 
